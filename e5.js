d955eb26276e095cc7244b5e0fc801a9ca200a63
gitbrasedu <127907909+gitbrasedu@users.noreply.github.com>
2023-06-13 12:34:20 -0300

    Add files via upload

---
+++ e5.js
@@ -0,0 +1,16 @@
+function contarVogais(str)
+{
+    const VOGAIS = ["a", "e", "i", "o", "u"];
+
+    /*
+        Vê se cada vogal está na lista
+    */
+    let nVogais = 0;
+    str.toLowerCase().forEach(letra => {
+        
+    });
+
+}
+
+
+console.log(contarVogais("cinco vogais"))
